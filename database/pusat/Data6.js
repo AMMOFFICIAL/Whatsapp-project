91889b55ba3c9f349006e8d9e015b22c0d432633
AMMOFFICIAL <akunilham296@gmail.com>
2026-08-19 11:38:56 +0700

    Add files via upload

---
+++ database/pusat/Data6.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch')
+const FormData = require('form-data')
+const { fromBuffer } = require('file-type')
+
+
+module.exports = async buffer => {
+  let { ext } = await fromBuffer(buffer);
+  let bodyForm = new FormData();
+  bodyForm.append("fileToUpload", buffer, "file." + ext);
+  bodyForm.append("reqtype", "fileupload");
+
+  let res = await fetch("https://catbox.moe/user/api.php", {
+    method: "POST",
+    body: bodyForm,
+  });
+
+  let data = await res.text();
+  return data;
+}
